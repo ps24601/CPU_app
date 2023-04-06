ba90c1e5945788f0687781641d994234184728e5
prashant <prashant.singh@giz.de>
2023-04-06 11:54:36 +0200

    datafile path update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
         package_dir={"": "src"},
         packages=setuptools.find_packages(where='src'),  
         package_data={
-        "appstore":['data/*.png','data/*.txt','data/*.json']},
+        "cpu_appstore":['data/*.png','data/*.txt','data/*.json']},
         # data_files=[('ndcs',['src/appstore/docStore/ndcs/cca.txt','src/appstore/docStore/ndcs/ccm.txt',
         # 'src/appstore/docStore/ndcs/countryList.txt'])],
         install_requires=install_requires, #external packages as dependencies
