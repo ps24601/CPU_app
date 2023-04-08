92351abff0e7fe3c62d0b86f5eeb7b19658ab404
prashant <prashant.singh@giz.de>
2023-04-08 12:26:05 +0200

    adding classifier template

---
+++ src/cpu_appstore/classifier_base.py
@@ -0,0 +1,177 @@
+from haystack.nodes import TransformersDocumentClassifier
+from haystack.schema import Document
+from typing import List, Tuple, Dict
+from typing_extensions import Literal
+import logging
+import pandas as pd
+from pandas import DataFrame, Series
+from utils.checkconfig import getconfig
+from utils.streamlitcheck import check_streamlit
+from utils.preprocessing import processingpipeline
+
+try:
+    import streamlit as st
+except ImportError:
+    logging.info("Streamlit not installed")
+
+# Declare all the necessary variables
+def get_classifier_params(model_name):
+    config = getconfig('paramconfig.cfg')
+    params = {}
+    params['model_name'] = config.get('sdg','MODEL')
+    params['split_by'] = config.get('sdg','SPLIT_BY')
+    params['split_length'] = int(config.get('sdg','SPLIT_LENGTH'))
+    params['split_overlap'] = int(config.get('sdg','SPLIT_OVERLAP'))
+    params['remove_punc'] = bool(int(config.get('sdg','REMOVE_PUNC')))
+    params['split_respect_sentence_boundary'] = bool(int(config.get('sdg','RESPECT_SENTENCE_BOUNDARY')))
+    params['threshold'] = float(config.get('sdg','THRESHOLD'))
+    params['top_n'] = int(config.get('sdg','TOP_KEY'))
+
+    return params
+
+@st.cache(allow_output_mutation=True)
+def para_classification(haystack_doc:List[Document],_lab_dict:Dict,
+                        threshold:float = 0.8,param_val = None, 
+                        classifier_model:TransformersDocumentClassifier= None
+                        )->Tuple[DataFrame,Series]:
+    """
+    Text-Classification on the list of texts provided. Classifier provides the 
+    most appropriate label for each text. these labels are in terms of if text 
+    belongs to which particular Sustainable Devleopment Goal (SDG).
+
+    Params
+    ---------
+    haystack_doc: List of haystack Documents. The output of Preprocessing Pipeline 
+    contains the list of paragraphs in different format,here the list of 
+    Haystack Documents is used.
+    threshold: threshold value for the model to keep the results from classifier
+    classifiermodel: you can pass the classifier model directly,which takes priority
+    however if not then looks for model in streamlit session.
+    In case of streamlit avoid passing the model directly.
+
+
+    Returns
+    ----------
+    df: Dataframe with two columns['SDG:int', 'text']
+    x: Series object with the unique SDG covered in the document uploaded and 
+    the number of times it is covered/discussed/count_of_paragraphs. 
+
+    """
+    logging.info("Working on SDG Classification")
+    if not classifier_model:
+        if check_streamlit():
+            classifier_model = st.session_state['{}_classifier'.format(param_val)]
+        else:
+            logging.warning("""No streamlit envinornment found or pass correct 
+                            param_val to look in session_state or Pass the classifier object""")
+            return
+    
+    results = classifier_model.predict(haystack_doc)
+
+
+    labels_= [(l.meta['classification']['label'],
+            l.meta['classification']['score'],l.content,) for l in results]
+
+    df = DataFrame(labels_, columns=["Target Label","Relevancy","text"])
+    
+    df = df.sort_values(by="Relevancy", ascending=False).reset_index(drop=True)  
+    df.index += 1
+    df =df[df['Relevancy']>threshold]
+
+    # creating the dataframe for value counts of SDG, along with 'title' of SDGs
+    x = df['Target Label'].value_counts()
+    x = x.rename('count')
+    x = x.rename_axis('Target Label').reset_index()
+    x["Target Label"] = pd.to_numeric(x["Target Label"])
+    x = x.sort_values(by=['count'], ascending=False)
+    x['TARGET_name'] = x['Target Label'].apply(lambda x: _lab_dict[x])
+    x['TARGET_Num'] = x['Target Label'].apply(lambda x: "TARGET LABEL "+str(x))
+
+    df['TARGET LABEL'] = pd.to_numeric(df['TARGET LABEL'])
+    df = df.sort_values('TARGET LABEL')
+
+    return df, x
+
+@st.cache(allow_output_mutation=True)
+def load_Classifier(config_file:str = None,param_val = None, 
+                    classifier_name:str = None):
+    """
+    loads the document classifier using haystack, where the name/path of model
+    in HF-hub as string is used to fetch the model object.Either configfile or 
+    model should be passed.
+    1. https://docs.haystack.deepset.ai/reference/document-classifier-api
+    2. https://docs.haystack.deepset.ai/docs/document_classifier
+
+    Params
+    --------
+    config_file: config file path from which to read the model name
+    classifier_name: if modelname is passed, it takes a priority if not \
+    found then will look for configfile, else raise error.
+
+
+    Return: document classifier model
+    """
+    if not classifier_name:
+        if not config_file:
+            logging.warning("Pass either model name or config file")
+            return
+        else:
+            config = getconfig(config_file)
+            classifier_name = config.get(param_val,'MODEL')
+    
+    logging.info("Loading classifier")    
+    doc_classifier = TransformersDocumentClassifier(
+                        model_name_or_path=classifier_name,
+                        task="text-classification")
+
+    return doc_classifier
+
+
+def runPreprocessingPipeline(file_name:str, file_path:str, 
+            split_by: Literal["sentence", "word"] = 'word',
+            split_length:int = 60, split_respect_sentence_boundary:bool = False,
+            split_overlap:int = 10,remove_punc:bool = False)->List[Document]:
+    """
+    creates the pipeline and runs the preprocessing pipeline, 
+    the params for pipeline are fetched from paramconfig
+
+    Params
+    ------------
+
+    file_name: filename, in case of streamlit application use 
+    st.session_state['filename']
+    file_path: filepath, in case of streamlit application use st.session_state['filepath']
+    split_by: document splitting strategy either as word or sentence
+    split_length: when synthetically creating the paragrpahs from document,
+                    it defines the length of paragraph.
+    split_respect_sentence_boundary: Used when using 'word' strategy for 
+    splititng of text.
+    split_overlap: Number of words or sentences that overlap when creating
+        the paragraphs. This is done as one sentence or 'some words' make sense
+        when  read in together with others. Therefore the overlap is used.
+    remove_punc: to remove all Punctuation including ',' and '.' or not
+
+
+    Return
+    --------------
+    List[Document]: When preprocessing pipeline is run, the output dictionary 
+    has four objects. For the Haysatck implementation of SDG classification we, 
+    need to use the List of Haystack Document, which can be fetched by 
+    key = 'documents' on output.
+
+    """
+
+    classifier_processing_pipeline = processingpipeline()
+
+    output_sdg_pre = classifier_processing_pipeline.run(file_paths = file_path, 
+                            params= {"FileConverter": {"file_path": file_path, \
+                                        "file_name": file_name}, 
+                                     "UdfPreProcessor": {"remove_punc": remove_punc, \
+                                            "split_by": split_by, \
+                                            "split_length":split_length,\
+                                            "split_overlap": split_overlap, \
+        "split_respect_sentence_boundary":split_respect_sentence_boundary}})
+    
+    return output_sdg_pre
+
+    
---
+++ src/cpu_appstore/target_classifier_app.py
@@ -0,0 +1,193 @@
+# set path
+import glob, os, sys
+
+#import needed libraries
+from haystack.nodes import TransformersDocumentClassifier
+from haystack.schema import Document
+from typing import List, Tuple
+from typing_extensions import Literal
+import logging
+import pandas as pd
+from pandas import DataFrame, Series
+from utils.checkconfig import getconfig
+from utils.streamlitcheck import check_streamlit
+from utils.preprocessing import processingpipeline
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid
+from st_aggrid.shared import ColumnsAutoSizeMode
+# from utils.sdg_classifier import sdg_classification
+from cpu_appstore.classifier_base import load_Classifier, runPreprocessingPipeline
+from cpu_appstore.classifier_base import para_classification, get_classifier_params
+from utils.keyword_extraction import textrank
+import logging
+logger = logging.getLogger(__name__)
+
+classifier_identifier = 'target_economy'
+
+params  = get_classifier_params(classifier_identifier)
+
+## Labels dictionary ###
+_lab_dict = {
+            'ECONOMY-WIDE':'ECONOMY-WIDE',
+            'NEGATIVE':'NO TARGET INFO',
+            }
+
+
+def app():
+
+    #### APP INFO #####
+    with st.container():
+        st.markdown("<h1 style='text-align: center; color: black;'> Targets Extraction </h1>", unsafe_allow_html=True)
+        st.write(' ')
+        st.write(' ')
+
+    with st.expander("ℹ️ - About this app", expanded=False):
+
+        st.write(
+            """     
+            The **Target Extraction** app is an easy-to-use interface built \
+                in Streamlit for analyzing policy documents for \
+                 Classification of the paragraphs/texts in the document *If it \
+                contains any Targets related information* and then further classify those \
+                targets - developed by GIZ Data Service Center, GFA, IKI Tracs, \
+                 SV Klima and SPA. \n
+            """)
+        st.write("""**Document Processing:** The Uploaded/Selected document is \
+            automatically cleaned and split into paragraphs with a maximum \
+            length of 60 words using a Haystack preprocessing pipeline. The \
+            length of 60 is an empirical value which should reflect the length \
+            of a “context” and should limit the paragraph length deviation. \
+            However, since we want to respect the sentence boundary the limit \
+            can breach and hence this limit of 60 is tentative.  \n
+            """)
+        st.write("""**SDG cLassification:** The application assigns paragraphs \
+            to 15 of the 17 United Nations Sustainable Development Goals (SDGs).\
+            SDG 16 “Peace, Justice and Strong Institutions” and SDG 17 \
+            “Partnerships for the Goals” are excluded from the analysis due to \
+            their broad nature which could potentially inflate the results. \
+            Each paragraph is assigned to one SDG only. Again, the results are \
+            displayed in a summary table including the number of the SDG, a \
+            relevancy score highlighted through a green color shading, and the \
+            respective text of the analyzed paragraph. Additionally, a pie \
+            chart with a blue color shading is displayed which illustrates the \
+            three most prominent SDGs in the document. The SDG classification \
+            uses open-source training [data](https://zenodo.org/record/5550238#.Y25ICHbMJPY) \
+            from [OSDG.ai](https://osdg.ai/) which is a global \
+            partnerships and growing community of researchers and institutions \
+            interested in the classification of research according to the \
+            Sustainable Development Goals. The summary table only displays \
+            paragraphs with a calculated relevancy score above 85%.  \n""")
+
+        st.write("""**Keyphrase Extraction:** The application extracts 15 \
+            keyphrases from the document, for each SDG label and displays the \
+            results in a summary table. The keyphrases are extracted using \
+            using [Textrank](https://github.com/summanlp/textrank)\
+            which is an easy-to-use computational less expensive \
+            model leveraging combination of TFIDF and Graph networks.
+            """)
+        st.write("")
+        st.write("")
+        st.markdown("Some runtime metrics tested with cpu: Intel(R) Xeon(R) CPU @ 2.20GHz, memory: 13GB")
+        col1,col2,col3,col4 = st.columns([2,2,4,4])
+        with col1:
+            st.caption("Loading Time Classifier")
+            # st.markdown('<div style="text-align: center;">12 sec</div>', unsafe_allow_html=True)
+            st.write("12 sec")
+        with col2:
+            st.caption("OCR File processing")
+            # st.markdown('<div style="text-align: center;">50 sec</div>', unsafe_allow_html=True)
+            st.write("50 sec")
+        with col3:
+            st.caption("SDG Classification of 200 paragraphs(~ 35 pages)")
+            # st.markdown('<div style="text-align: center;">120 sec</div>', unsafe_allow_html=True)
+            st.write("120 sec")
+        with col4:
+            st.caption("Keyword extraction for 200 paragraphs(~ 35 pages)")
+            # st.markdown('<div style="text-align: center;">3 sec</div>', unsafe_allow_html=True)
+            st.write("3 sec")
+
+        
+
+    
+    ### Main app code ###
+    with st.container():
+        if st.button("RUN Target Related Paragraph Extractions"):
+                   
+            if 'filepath' in st.session_state:
+                file_name = st.session_state['filename']
+                file_path = st.session_state['filepath']
+                classifier = load_Classifier(classifier_name=params['model_name'])
+                st.session_state[classifier_identifier] = classifier
+                all_documents = runPreprocessingPipeline(file_name= file_name,
+                                        file_path= file_path, split_by= params['split_by'],
+                                        split_length= params['split_length'],
+                split_respect_sentence_boundary= params['split_respect_sentence_boundary'],
+                split_overlap= params['split_overlap'], remove_punc= params['remove_punc'])
+
+                if len(all_documents['documents']) > 100:
+                    warning_msg = ": This might take sometime, please sit back and relax."
+                else:
+                    warning_msg = ""
+
+                with st.spinner("Running Target Related Paragraph Extractions{}".format(warning_msg)):
+
+                    df, x = para_classification(haystack_doc=all_documents['documents'],
+                                                _lab_dict = _lab_dict,
+                                                threshold= params['threshold'], 
+                                                classifier_model=st.session_state[classifier_identifier])
+                    df = df.drop(['Relevancy'], axis = 1)
+
+                    target_labels = x['Target Label'].unique()
+                    textrank_keyword_list = []
+                    for label in target_labels:
+                        classifier_data = " ".join(df[df['Target Label'] == label].text.to_list())
+                        textranklist_ = textrank(textdata=classifier_data, words= params['top_n'])
+                        if len(textranklist_) > 0:
+                            textrank_keyword_list.append({'SDG':label, 'TextRank Keywords':",".join(textranklist_)})
+                    textrank_keywords_df = pd.DataFrame(textrank_keyword_list)
+
+
+                    plt.rcParams['font.size'] = 25
+                    colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(x)))
+                    # plot
+                    fig, ax = plt.subplots()
+                    ax.pie(x['count'], colors=colors, radius=2, center=(4, 4),
+                        wedgeprops={"linewidth": 1, "edgecolor": "white"},
+                        textprops={'fontsize': 14}, 
+                        frame=False,labels =list(x.SDG_Num),
+                        labeldistance=1.2)
+                    # fig.savefig('temp.png', bbox_inches='tight',dpi= 100)
+                    
+
+                    st.markdown("#### Anything related to SDGs? ####")
+
+                    c4, c5, c6 = st.columns([1,2,2])
+
+                    with c5:
+                        st.pyplot(fig)
+                    with c6:
+                        labeldf = x['TARGET_name'].values.tolist()
+                        labeldf = "<br>".join(labeldf)
+                        st.markdown(labeldf, unsafe_allow_html=True)
+                    st.write("")
+                    st.markdown("###### What keywords are present under Economy Wide classified text? ######")
+
+                    AgGrid(textrank_keywords_df, reload_data = False, 
+                            update_mode="value_changed",
+                    columns_auto_size_mode = ColumnsAutoSizeMode.FIT_CONTENTS)
+                    st.write("")
+                    st.markdown("###### Top few Economy Wide Target Classified paragraph/text results ######")
+
+                    AgGrid(df, reload_data = False, update_mode="value_changed",
+                    columns_auto_size_mode = ColumnsAutoSizeMode.FIT_CONTENTS)
+            else:
+                st.info("🤔 No document found, please try to upload it at the sidebar!")
+                logging.warning("Terminated as no document provided")
+
+
+
+
